303fb4362cfb8277169f9e6b3bc64bb6e356e6f3
Jerry Kim <ksyjerry@naver.com>
2024-12-29 22:40:27 +0900

    gogogo

--- program.py
+++ program.py
@@ -3,7 +3,7 @@ import random
 print("Hello World")
 
 def game():
-    print("가위바위보 게임을 시작합니다.")
+    print("묵찌빠 게임을 시작합니다.")
     print("가위바위보 중 하나를 입력하세요")
     user = input()
     computer = random.randint(0, 2)
